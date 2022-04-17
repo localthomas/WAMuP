687e2c7d8e68960709928f9d5496a744beb68f65
localthomas <81923579+localthomas@users.noreply.github.com>
2022-04-17 18:44:14 +0200

    refactored loudness computations into their own module

---
+++ src/miscellaneous/audio.ts
@@ -0,0 +1,35 @@
+import connectAudioEBUR128Prefilter from "../player/audio-ebur128-prefilter";
+
+/**
+ * Decode a binary file as audio data and apply the EBU R128 filter.
+ * This means the resulting audio data is transformed via various filters and
+ * is meant for analysis and not listening.
+ * @param data the raw binary audio file
+ * @returns the audio buffer filled with audio PCM data
+ */
+export async function getAudioBufferFromBlobWithEBUR128Filter(data: Blob): Promise<AudioBuffer> {
+    // use decodeAudioCtx only for decoding the audio data
+    const decodeAudioCtx = new OfflineAudioContext(1, 1024, 44100);
+    console.time("TEST decode audio data");
+    const buffer = await decodeAudioCtx.decodeAudioData(await data.arrayBuffer());
+    console.timeEnd("TEST decode audio data");
+
+    // create a new audio context with the correct values from the decoded audio data
+    const offlineAudio = new OfflineAudioContext(
+        buffer.numberOfChannels, buffer.length, buffer.sampleRate);
+
+    // create input buffer
+    const bufferSource = offlineAudio.createBufferSource();
+    bufferSource.buffer = buffer;
+
+    // pre-filter the data
+    connectAudioEBUR128Prefilter(offlineAudio, bufferSource, offlineAudio.destination);
+
+    // let the filter run and use the result to calculate values
+    console.time("TEST offline rendering");
+    bufferSource.start();
+    const resultBuffer = await offlineAudio.startRendering();
+    console.timeEnd("TEST offline rendering");
+
+    return resultBuffer;
+}
