b3cedba50af9bf868ee2a0692ada264a083245cc
localthomas <81923579+localthomas@users.noreply.github.com>
2022-04-23 14:14:09 +0200

    added styling to spectrum component

--- src/components/spectrum.tsx
+++ src/components/spectrum.tsx
@@ -65,45 +65,43 @@ export default function Spectrum(props: {
         });
     });
     return (
-        <div>
-            <div class="columns">
-                <div class="column has-text-centered is-full">
-                    <canvas class="spectrum-canvas" ref={canvasRef}></canvas>
-                    <p class="mb-5">
-                        {"Max. displayed frequency: " + MAX_FREQ / 1000 + "kHz. Scale is linear."}
-                    </p>
-                    <div class="field">
-                        <label class="label">Smoothing (averaging constant with the last frame)</label>
-                        <div class="control">
-                            <input style={{ "width": "75%" }} type="range" min="0" max="0.99" step="0.01" value={smoothing()}
-                                onChange={event => {
-                                    setSmoothing(parseFloat(event.currentTarget.value));
-                                }}
-                                onDblClick={() => {
-                                    setSmoothing(DEFAULT_SMOOTHING);
-                                }} />
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Sampling size (resolution of spectrum): {
-                            /* calculate num of shown bars, which is the fftSize/2 * MAX_FREQ/nyquist */
-                            fftSize() * (MAX_FREQ / props.audioSession.getSampleRate())
-                        }</label>
-                        <div class="control">
-                            <PlusBtn onClick={() => {
-                                if (fftSize() < MAX_FFT_SIZE) {
-                                    setFftSize(fftSize() * 2);
-                                }
-                            }} disabled={fftSize() >= MAX_FFT_SIZE} />
-                            <MinusBtn onClick={() => {
-                                if (fftSize() > MIN_FFT_SIZE) {
-                                    setFftSize(fftSize() / 2);
-                                }
-                            }} disabled={fftSize() <= MIN_FFT_SIZE} />
-                        </div>
-                    </div>
-                </div>
+        <>
+            <canvas class="spectrum-canvas" ref={canvasRef}></canvas>
+            <p class="spectrum-canvas-label">
+                {"Max. displayed frequency: " + MAX_FREQ / 1000 + "kHz. Scale is linear."}
+            </p>
+            <div class="spectrum-settings">
+                <fieldset>
+                    <legend>Smoothing (averaging constant with the last frame)</legend>
+                    <input type="range" min="0" max="0.99" step="0.01" value={smoothing()}
+                        onChange={event => {
+                            setSmoothing(parseFloat(event.currentTarget.value));
+                        }}
+                        onDblClick={() => {
+                            setSmoothing(DEFAULT_SMOOTHING);
+                        }} />
+                </fieldset>
+                <fieldset>
+                    <legend>Sampling size (resolution of spectrum): {
+                        /* calculate num of shown bars, which is the fftSize/2 * MAX_FREQ/nyquist */
+                        fftSize() * (MAX_FREQ / props.audioSession.getSampleRate())
+                    }</legend>
+                    <PlusBtn
+                        small
+                        onClick={() => {
+                            if (fftSize() < MAX_FFT_SIZE) {
+                                setFftSize(fftSize() * 2);
+                            }
+                        }} disabled={fftSize() >= MAX_FFT_SIZE} />
+                    <MinusBtn
+                        small
+                        onClick={() => {
+                            if (fftSize() > MIN_FFT_SIZE) {
+                                setFftSize(fftSize() / 2);
+                            }
+                        }} disabled={fftSize() <= MIN_FFT_SIZE} />
+                </fieldset>
             </div>
-        </div>
+        </>
     );
 }
