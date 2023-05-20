45322d441dc0d8a5cda306b75c0ff61ca1495681
localthomas <localthomas@users.noreply.github.com>
2023-05-20 13:02:07 +0200

    bugfix: AssetDetailed was not updated on asset change

--- src/components/asset-detailed.tsx
+++ src/components/asset-detailed.tsx
@@ -1,4 +1,4 @@
-import { children, createResource, ParentComponent } from "solid-js";
+import { children, createMemo, createResource, ParentComponent } from "solid-js";
 import { BackendStore } from "../backend/backend";
 import { blobToImageSrcWithDefault } from "../miscellaneous/images";
 
@@ -10,14 +10,18 @@ export type AssetDetailedProps = {
 export const AssetDetailed: ParentComponent<AssetDetailedProps> = (props) => {
     const resolvedChildren = children(() => props.children);
 
-    const asset = props.backend.get(props.assetID);
-    if (!asset) {
-        throw "unreachable";
-    }
-    const metadata = asset.metadata;
+    const assetID = createMemo(() => props.assetID);
 
-    const [thumbnail] = createResource(async () => {
-        return await props.backend.getThumbnail(props.assetID);
+    const metadata = createMemo(() => {
+        const asset = props.backend.get(assetID());
+        if (!asset) {
+            throw "unreachable";
+        }
+        return asset.metadata;
+    });
+
+    const [thumbnail] = createResource(assetID, async (assetID) => {
+        return await props.backend.getThumbnail(assetID);
     });
 
     return (
@@ -26,41 +30,41 @@ export const AssetDetailed: ParentComponent<AssetDetailedProps> = (props) => {
                 <img src={blobToImageSrcWithDefault(thumbnail())} loading="lazy" />
             </div>
             <div class="column details">
-                <h1>{metadata.title}</h1>
-                <h2>{metadata.artist}</h2>
+                <h1>{metadata().title}</h1>
+                <h2>{metadata().artist}</h2>
                 <table>
                     <tbody>
                         <tr>
                             <td>Album:</td>
-                            <td>{metadata.album}</td>
+                            <td>{metadata().album}</td>
                         </tr>
                         <tr>
                             <td>Album Artist:</td>
-                            <td>{metadata.albumArtist}</td>
+                            <td>{metadata().albumArtist}</td>
                         </tr>
                         <tr>
                             <td>Composer:</td>
-                            <td>{metadata.composer}</td>
+                            <td>{metadata().composer}</td>
                         </tr>
                         <tr>
                             <td>Genre:</td>
-                            <td>{metadata.genre}</td>
+                            <td>{metadata().genre}</td>
                         </tr>
                         <tr>
                             <td>Year:</td>
-                            <td>{metadata.year}</td>
+                            <td>{metadata().year}</td>
                         </tr>
                         <tr>
                             <td>Track:</td>
-                            <td>{metadata.track.no}/{metadata.track.of}</td>
+                            <td>{metadata().track.no}/{metadata().track.of}</td>
                         </tr>
                         <tr>
                             <td>Disc:</td>
-                            <td>{metadata.disc.no}/{metadata.disc.of}</td>
+                            <td>{metadata().disc.no}/{metadata().disc.of}</td>
                         </tr>
                         <tr>
                             <td>Codec:</td>
-                            <td>{metadata.codec}</td>
+                            <td>{metadata().codec}</td>
                         </tr>
                     </tbody>
                 </table>
