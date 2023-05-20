307a90b300f364192319b4bdbca7e02ff0f657ee
localthomas <localthomas@users.noreply.github.com>
2023-05-20 16:43:45 +0200

    added data persistence request

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,17 @@ import { hashIntegration, Router } from "@solidjs/router";
 import './index.css';
 import App from './components/App';
 
+// request persistent storage from the browser to keep the cache (IndexedDB) between restarts
+async function persistData() {
+    if (navigator.storage && navigator.storage.persist) {
+        if (!await navigator.storage.persisted()) {
+            const result = await navigator.storage.persist();
+            console.log(`Data persisted: ${result}`);
+        }
+    }
+}
+persistData();
+
 render(() => (
     <Router source={hashIntegration()}>
         <App />
